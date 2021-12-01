3ecf4f836de7b8270f43821f70427846c9073230
YdrMaster <ydrml@hotmail.com>
2021-12-01 14:31:20 +0800

    feat: 实现虚拟激光雷达

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use async_std::{net::UdpSocket, sync::Arc, task};
 use monitor_tool::{rgba, vertex, Encoder, Shape, Vertex};
 use parry2d::na::{Isometry2, Point2, Vector2};
 use path_tracking::Tracker;
-use pm1_control_model::{Odometry, Optimizer, Physical, Pm1Predictor, TrajectoryPredictor};
+use pm1_control_model::{Odometry, Optimizer, Pm1Predictor, TrajectoryPredictor};
 use std::{
-    f32::consts::{FRAC_PI_3, FRAC_PI_8, PI},
+    f32::consts::{FRAC_PI_2, FRAC_PI_8, PI},
     time::{Duration, Instant},
 };
 
@@ -12,9 +12,7 @@ mod chassis;
 mod obstacles;
 
 use chassis::{rgbd_bounds, CHASSIS_TOPIC, ODOMETRY_TOPIC, ROBOT_OUTLINE, RUDDER, SIMPLE_OUTLINE};
-use obstacles::TRICYCLE_OUTLINE;
-
-use crate::obstacles::OBSTACLES_TOPIC;
+use obstacles::{ray_cast, LIDAR_TOPIC, OBSTACLES_TOPIC, TRICYCLE_OUTLINE};
 
 macro_rules! vertex_from_pose {
     ($level:expr; $pose:expr; $alpha:expr) => {
@@ -27,9 +25,10 @@ macro_rules! vertex_from_pose {
 }
 
 macro_rules! pose {
-    ($x:expr, $y:expr; $theta:expr) => {
-        Isometry2::new(Vector2::new($x, $y), $theta)
-    };
+    ($x:expr, $y:expr; $theta:expr) => {{
+        let (sin, cos) = ($theta as f32).sin_cos();
+        pm1_control_model::isometry($x, $y, cos, sin)
+    }};
 }
 
 const FOCUS_TOPIC: &str = "focus";
@@ -37,8 +36,11 @@ const LIGHT_TOPIC: &str = "light";
 const PATH_TOPIC: &str = "path";
 const PRE_TOPIC: &str = "pre";
 
-const FF: f32 = 3.0; // 倍速仿真
+const FF: f32 = 1.0; // 倍速仿真
 const PATH_TO_TRACK: &str = "1105-1"; // 路径名字
+
+const RGBD_METERS: f32 = 4.0;
+const RGBD_DEGREES: f32 = 51.0;
 
 fn main() {
     task::block_on(async {
@@ -50,7 +52,7 @@ fn main() {
         let mut odometry = Odometry {
             s: 0.0,
             a: 0.0,
-            pose: pose!(-7686.5, -1871.5; 0.0),
+            pose: pose!(-7686.0, -1875.0; -FRAC_PI_2),
         };
         // 底盘运动仿真
         let mut predictor = TrajectoryPredictor::<Pm1Predictor> {
@@ -58,28 +60,27 @@ fn main() {
             model: Default::default(),
             predictor: Pm1Predictor::new(Optimizer::new(0.5, 1.2, PERIOD), PERIOD),
         };
-        predictor.predictor.target = Physical {
-            speed: 0.5,
-            rudder: FRAC_PI_8,
-        };
         // 深度相机
-        let rgbd = rgbd_bounds(4.0, 85.0);
-        let tricicle = {
-            let tr = pose!(-7685.5, -1880.0; FRAC_PI_3);
-            TRICYCLE_OUTLINE
-                .iter()
-                .map(|v| transform(&tr, *v))
-                .collect::<Vec<_>>()
+        let rgbd = rgbd_bounds(RGBD_METERS, RGBD_DEGREES);
+        let tricycle = {
+            let tr = pose!(-7685.5, -1880.0; -0.0);
+            TRICYCLE_OUTLINE.iter().map(|v| tr * v).collect::<Vec<_>>()
         };
+        let obstables = vec![tricycle];
         // 发送固定物体
         let path = repos.read(PATH_TO_TRACK).unwrap();
-        send_config(socket.clone(), Duration::from_secs(3), path, vec![tricicle]);
+        send_config(
+            socket.clone(),
+            Duration::from_secs(3),
+            path,
+            obstables.clone(),
+        );
         // 循线仿真
         let _ = repos.track(PATH_TO_TRACK);
         let mut time = Instant::now();
         loop {
             // 更新
-            odometry += predictor.next().unwrap();
+            predictor.next().map(|d| odometry += d);
             // 循线
             let ref mut target = predictor.predictor.target;
             if let Some((speed, rudder)) = repos.put_pose(&odometry.pose) {
@@ -92,32 +93,43 @@ fn main() {
             let socket = socket.clone();
             let predictor = predictor.clone();
             let rgbd = rgbd.clone();
+            let lidar = ray_cast(odometry.pose, &obstables, RGBD_METERS, RGBD_DEGREES);
             task::spawn(async move {
                 let tr = odometry.pose;
-                let packet = Encoder::with(|encoder| {
-                    encoder
-                        .topic(ODOMETRY_TOPIC)
-                        .push(vertex_from_pose!(0; odometry.pose; 0));
-                    encoder.with_topic(CHASSIS_TOPIC, |mut chassis| {
-                        chassis.clear();
-                        chassis.extend(ROBOT_OUTLINE.iter().map(|v| transform(&tr, *v)));
-                        chassis.extend(SIMPLE_OUTLINE.iter().map(|v| transform(&tr, *v)));
-                        chassis.extend(rgbd.iter().map(|v| transform(&tr, *v)));
-
-                        let tr = tr * pose!(-0.355, 0.0; predictor.predictor.current.rudder);
-                        chassis.extend(RUDDER.iter().map(|v| transform(&tr, *v)));
-                    });
-                    encoder.with_topic(FOCUS_TOPIC, |mut light| {
+                let packet = Encoder::with(|figure| {
+                    figure.with_topic(FOCUS_TOPIC, |mut light| {
                         light.clear();
                         light.push(transform(&tr, vertex!(0; 2.0, 0.0; Circle, 3.0; 0)));
                     });
-                    encoder.with_topic(LIGHT_TOPIC, |mut light| {
-                        light.clear();
-                        light.push(transform(&tr, vertex!(0; 0.4, 0.0; Circle, 0.4; 0)));
+
+                    figure
+                        .topic(ODOMETRY_TOPIC)
+                        .push(vertex_from_pose!(0; odometry.pose; 0));
+                    figure.with_topic(CHASSIS_TOPIC, |mut topic| {
+                        topic.clear();
+                        topic.extend(ROBOT_OUTLINE.iter().map(|v| transform(&tr, *v)));
+                        topic.extend(SIMPLE_OUTLINE.iter().map(|v| transform(&tr, *v)));
+                        topic.extend(rgbd.iter().map(|v| transform(&tr, *v)));
+
+                        let tr = tr * pose!(-0.355, 0.0; predictor.predictor.current.rudder);
+                        topic.extend(RUDDER.iter().map(|v| transform(&tr, *v)));
+                    });
+                    figure.with_topic(LIGHT_TOPIC, |mut topic| {
+                        topic.clear();
+                        topic.push(transform(&tr, vertex!(0; 0.4, 0.0; Circle, 0.4; 0)));
+                    });
+                    figure.with_topic(LIDAR_TOPIC, |mut topic| {
+                        topic.clear();
+                        topic.extend(
+                            lidar
+                                .iter()
+                                .map(|p| tr * p.to_point())
+                                .map(|p| vertex!(0; p[0], p[1]; 0)),
+                        );
                     });
                     // 轨迹预测
-                    encoder.with_topic(PRE_TOPIC, |mut pre| {
-                        pre.clear();
+                    figure.with_topic(PRE_TOPIC, |mut topic| {
+                        topic.clear();
                         let period = predictor.period;
                         let mut t = Duration::ZERO;
                         let mut s = odometry.s + 0.1;
@@ -132,7 +144,7 @@ fn main() {
                             if odometry.s > s || odometry.a > a {
                                 s = odometry.s + 0.1;
                                 a = odometry.a + FRAC_PI_8;
-                                pre.push(vertex_from_pose!(0; odometry.pose; 0));
+                                topic.push(vertex_from_pose!(0; odometry.pose; 0));
                             }
                             if odometry.s > max_s || odometry.a > max_a || t > max_t {
                                 break;
@@ -152,7 +164,7 @@ fn main() {
 }
 
 fn transform(tr: &Isometry2<f32>, mut vertex: Vertex) -> Vertex {
-    let p = tr * Point2::new(vertex.x, vertex.y);
+    let p = tr * point(vertex.x, vertex.y);
     vertex.x = p[0];
     vertex.y = p[1];
     if vertex.shape == Shape::Arrow && vertex.extra.is_finite() {
@@ -165,7 +177,7 @@ fn send_config(
     socket: Arc<UdpSocket>,
     period: Duration,
     path: Vec<Isometry2<f32>>,
-    obstacles: impl IntoIterator<Item = Vec<Vertex>>,
+    obstacles: impl IntoIterator<Item = Vec<Point2<f32>>>,
 ) {
     let packet = Encoder::with(|encoder| {
         encoder.config_topic(
@@ -183,6 +195,7 @@ fn send_config(
         encoder.config_topic(ODOMETRY_TOPIC, 20000, 0, &[(0, rgba!(VIOLET; 0.1))], |_| {});
         encoder.config_topic(FOCUS_TOPIC, 1, 1, &[(0, rgba!(BLACK; 0.0))], |_| {});
         encoder.config_topic(LIGHT_TOPIC, 1, 0, &[(0, rgba!(RED; 1.0))], |_| {});
+        encoder.config_topic(LIDAR_TOPIC, 2000, 0, &[(0, rgba!(GOLD; 0.5))], |_| {});
         encoder.config_topic(PRE_TOPIC, 100, 0, &[(0, rgba!(SKYBLUE; 0.3))], |_| {});
         encoder.config_topic(
             OBSTACLES_TOPIC,
@@ -192,7 +205,10 @@ fn send_config(
             |mut encoder| {
                 encoder.clear();
                 for o in obstacles {
-                    encoder.extend(o.into_iter());
+                    if let Some(p) = o.last() {
+                        encoder.push(vertex!(0; p[0], p[1]; 0));
+                        encoder.extend(o.into_iter().map(|p| vertex!(0; p[0], p[1]; 64)));
+                    }
                 }
             },
         );
@@ -215,4 +231,17 @@ fn send_config(
             task::sleep(period).await;
         }
     });
+}
+
+#[inline]
+const fn point(x: f32, y: f32) -> Point2<f32> {
+    Point2 {
+        coords: vector(x, y),
+    }
+}
+
+#[inline]
+const fn vector(x: f32, y: f32) -> Vector2<f32> {
+    use parry2d::na::{ArrayStorage, OVector};
+    OVector::from_array_storage(ArrayStorage([[x, y]]))
 }
--- src/obstacles.rs
+++ src/obstacles.rs
@@ -1,13 +1,80 @@
-﻿use monitor_tool::{vertex, Vertex};
+﻿use super::{point, vector};
+use parry2d::{
+    bounding_volume::AABB,
+    na::Point2,
+    query::{Ray, RayCast},
+    shape::ConvexPolygon,
+};
+use pm1_control_model::Isometry2;
+use rand::{thread_rng, Rng};
+use std::f32::consts::PI;
 
 pub(super) const OBSTACLES_TOPIC: &str = "obstacles";
+pub(super) const LIDAR_TOPIC: &str = "lidar";
 
-pub(super) const TRICYCLE_OUTLINE: [Vertex; 6] = [
-    vertex!(0; 1.0, 0.0; 0),
-    vertex!(0; 0.0, 0.75; 64),
-    vertex!(0; -2.0, 0.75; 64),
-    vertex!(0; -2.0, -0.75; 64),
-    vertex!(0; 0.0, -0.75; 64),
-    //
-    vertex!(0; 1.0, 0.0; 64),
+pub(super) const TRICYCLE_OUTLINE: [Point2<f32>; 5] = [
+    point(1.0, 0.0),
+    point(0.0, 0.75),
+    point(-2.0, 0.75),
+    point(-2.0, -0.75),
+    point(0.0, -0.75),
 ];
+
+pub(super) struct Polar {
+    rho: f32,
+    theta: f32,
+}
+
+impl Polar {
+    pub fn to_point(&self) -> Point2<f32> {
+        let (sin, cos) = self.theta.sin_cos();
+        point(cos * self.rho, sin * self.rho)
+    }
+}
+
+pub(super) fn ray_cast(
+    pose: Isometry2<f32>,
+    obstacles: &Vec<Vec<Point2<f32>>>,
+    radius: f32,
+    degrees: f32,
+) -> Vec<Polar> {
+    // 转本地多边形障碍物
+    let obstacles = {
+        let x0 = pose.translation.vector[0];
+        let y0 = pose.translation.vector[1];
+        let aabb = AABB::new(point(x0 - 10.0, y0 - 10.0), point(x0 + 10.0, y0 + 10.0));
+        let to_local = pose.inverse();
+        obstacles
+            .iter()
+            .filter(|v| v.iter().any(|p| aabb.contains_local_point(p)))
+            .filter_map(|v| {
+                ConvexPolygon::from_convex_polyline(
+                    v.iter().map(|p| to_local * p).collect::<Vec<_>>(),
+                )
+            })
+            .collect::<Vec<_>>()
+    };
+    //
+    let mut result = Vec::new();
+    const ORIGIN: Point2<f32> = point(0.0, 0.0);
+    const STEP: f32 = PI / 360.0;
+    let dir_range = (degrees * 0.5).to_radians();
+    let mut theta = -dir_range;
+    while theta <= dir_range {
+        let (sin, cos) = theta.sin_cos();
+        let ray = Ray {
+            origin: ORIGIN,
+            dir: vector(cos, sin),
+        };
+        let rho = obstacles
+            .iter()
+            .filter_map(|c| c.cast_local_ray(&ray, radius, true))
+            .fold(f32::INFINITY, |min, l| f32::min(min, l))
+            + thread_rng().gen_range(-0.01..0.01);
+        if rho.is_normal() {
+            result.push(Polar { rho, theta });
+        }
+        theta += STEP;
+    }
+    result
+}
